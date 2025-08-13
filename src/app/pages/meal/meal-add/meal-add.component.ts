1da727f2cbf557345cd78095b39767bf4ab51878
Muzaffar <muzaffar.ipcs@gmail.com>
2025-08-13 17:13:38 +0530

    meal image added

--- src/app/pages/meal/meal-add/meal-add.component.ts
+++ src/app/pages/meal/meal-add/meal-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation, EventEmitter, ElementRef, ViewChi
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
 
 import { ToastrManager } from 'ng6-toastr-notifications';
 import { RoleService } from 'src/app/providers/role/role.service';
@@ -10,13 +11,42 @@ import { EmployeeService } from 'src/app/providers/employee/employee.service';
 import { DepartmentService } from 'src/app/providers/department/department.service';
 import { PositionService } from 'src/app/providers/position/position.service';
 import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { MediaService } from '../../../providers/media/media.service';
 @Component({
 	selector: 'app-meal-add',
 	templateUrl: './meal-add.component.html',
 	styleUrls: ['./meal-add.component.scss']
 })
 export class MealAddComponent implements OnInit {
+
+	@ViewChild('uploader', { read: ElementRef }) fileInput: ElementRef;
+	// File Upload
+	options: UploaderOptions;
+	uploadInput: EventEmitter<UploadInput>;
+	bannerImage: any;
+	bannerVideo: any;
 	imagePath: any;
+	imageArr: any = [];
+	mediaData: any;
+	images: any = [];
+
+	addmediaForm: FormGroup;
+	submittedMedia: boolean = false;
+	fileFormat: any;
+	temp_sequence_number = 0;
+	mediaFile: any;
+	isUploaded: boolean = false;
+
+	isMediaDeleted = false;
+	deletedMediaData: any;
+	isMediaFileDeleted = false;
+	deletedMediaFile: any = [];
+	isMediaEdit = false;
+	mediaID: any;
+	bannerData: any;
+	ImageName: any;
+
+
 	// Data Assign
 	addmealForm: FormGroup;
 	throw_msg: any;
@@ -58,7 +88,11 @@ export class MealAddComponent implements OnInit {
 		public employeeService: EmployeeService,
 		public positionService: PositionService,
 		private modalService: NgbModal,
+		private mediaService: MediaService,
 	) {
+		this.uploadInput = new EventEmitter<UploadInput>();
+		this.options = { concurrency: 0, allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'] };
+
 		this.addmealForm = this.formBuilder.group({
 			name: ['', Validators.required],
 			description: [''],
@@ -83,8 +117,26 @@ export class MealAddComponent implements OnInit {
 			employeesPerOrder: [''],
 		});
 		this.token = localStorage.getItem('albaik-admin-token');
-		this.imagePath = environment.baseUrl + '/public/';
+		this.imagePath = environment.baseUrl + '/public/meal/';
 		this.url = environment.Url + '/assets';
+		this.addmediaForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			status: [true, Validators.required],
+			sequence_number: [''],
+			src: ['', Validators.required],
+			format: [''],
+			file_type: ['image'],
+			alt: [''],
+			role: [''],
+			resolution: [''],
+			size: [''],
+			height: [''],
+			width: [''],
+			mute: ['muted'],
+			autoplay: [true],
+			loop: [true],
+			full_screen: [''],
+		});
 	}
 
 	ngOnInit(): void {
@@ -143,9 +195,11 @@ export class MealAddComponent implements OnInit {
 						});
 					});
 				}
-				if(data?.department_needs){
+				if (data?.department_needs) {
 					this.selectedDeptData = data?.department_needs;
 				}
+				this.mediaData = data?.media_data;
+				this.ImageName = data?.image;
 				// Patch values to form
 				this.addmealForm.patchValue({
 					name: data.name,
@@ -162,8 +216,10 @@ export class MealAddComponent implements OnInit {
 					meal_id: data?.meal_id,
 					machine: data?.machine,
 					package: data?.package,
+					image: data?.image,
+
 				});
-				
+
 			} else {
 
 			}
@@ -178,7 +234,9 @@ export class MealAddComponent implements OnInit {
 		if (this.addmealForm.invalid) {
 			return;
 		}
-
+		if (this.mediaData) {
+			obj['image'] = this.mediaData._id;
+		}
 		if (!this.isEdit) {
 			this.mealService.addMeal(obj).subscribe((response) => {
 				if (response.code === 200) {
@@ -358,4 +416,244 @@ export class MealAddComponent implements OnInit {
 	onCancel(): void {
 		this.router.navigate(['/meal/view']);
 	}
+
+	openMedia(content: any) {
+		this.addmediaForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			status: [true, Validators.required],
+			sequence_number: [''],
+			src: ['', Validators.required],
+			format: [''],
+			file_type: ['image'],
+			alt: [''],
+			role: [''],
+			resolution: [''],
+			size: [''],
+			height: [''],
+			width: [''],
+			mute: ['muted'],
+			autoplay: [true],
+			loop: [true],
+			full_screen: [''],
+		});
+		this.mediaFile = '';
+		this.isMediaEdit = false;
+		this.mediaID = '';
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', windowClass: "myCustomModalClass", size: 'xl', backdrop: 'static' })
+			.result.then((result) => {
+				this.closeResult = `Closed with: ${result}`;
+			}, (reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			});
+	}
+
+	editMedia(content: any, mediaData, type) {
+		this.isMediaEdit = true;
+		this.mediaFile = mediaData.src;
+		this.mediaID = mediaData._id;
+		this.addmediaForm.patchValue({
+			name: mediaData.name,
+			status: mediaData.status,
+			sequence_number: mediaData.sequence_number,
+			src: mediaData.src,
+			format: mediaData.format,
+			file_type: mediaData.file_type,
+			alt: mediaData.alt,
+			role: mediaData.role,
+			resolution: mediaData.resolution,
+			size: mediaData.size,
+			height: mediaData.height,
+			width: mediaData.width,
+			mute: mediaData.mute,
+			autoplay: mediaData.autoplay,
+			loop: mediaData.loop,
+			full_screen: mediaData.full_screen,
+		});
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', windowClass: "myCustomModalClass", size: 'xl', backdrop: 'static' })
+			.result.then((result) => {
+				this.closeResult = `Closed with: ${result}`;
+			}, (reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			});
+	}
+
+	onUploadOutput(output: UploadOutput, typeofImage): void {
+		if (output.type === 'allAddedToQueue') {
+			const event: UploadInput = {
+				type: 'uploadAll',
+				url: environment.baseUrl + '/api/meal/addimage',
+				method: 'POST',
+				data: {},
+			};
+			this.uploadInput.emit(event);
+		}
+		else if (output.type === 'done' && typeof output.file !== 'undefined') {
+			this.isUploaded = true;
+			this.fileFormat = output.file.type;
+			if (this.mediaFile) {
+				this.deletedMediaFile.push(this.mediaFile);
+				this.isMediaFileDeleted = true;
+			}
+			this.mediaFile = output.file.response.result;
+			this.addmediaForm.value.resolution = output.file.size;
+			this.submittedMedia = false;
+			this.addmediaForm.patchValue({
+				src: this.mediaFile
+			});
+		}
+	}
+
+	selectImageRole(event, role) {
+		if (role == 'base') {
+			this.addmediaForm.patchValue({
+				height: 1100,
+				width: 1100,
+			});
+		} else if (role == 'small') {
+			this.addmediaForm.patchValue({
+				height: 309,
+				width: 309,
+			});
+		} else if (role == 'thumbnail') {
+			this.addmediaForm.patchValue({
+				height: 150,
+				width: 150,
+			});
+		}
+	}
+
+	public hasMediaFormError = (controlName: string, errorName: string) => {
+		return this.addmediaForm.controls[controlName].hasError(errorName);
+	};
+
+	onSubmitMedia(type) {
+		let obj = this.addmediaForm.value;
+		let id = this.mediaID;
+		obj['token'] = this.token;
+		obj['src'] = this.mediaFile;
+		obj['format'] = this.fileFormat;
+		this.submittedMedia = true;
+		if (this.addmediaForm.invalid) {
+			return;
+		}
+		if (!this.isMediaEdit) {
+			this.mediaService.addMedia(obj).subscribe(
+				(response) => {
+					if (response.code == 200) {
+						this.submittedMedia = false;
+						if (this.deletedMediaFile.length > 0) {
+							this.deleteMediaFile();
+						}
+						this.toastr.successToastr(response.message);
+						if (this.addmediaForm.value.sequence_number) {
+							this.temp_sequence_number = this.addmediaForm.value.sequence_number
+						} else {
+							this.temp_sequence_number = this.temp_sequence_number + 1;
+						}
+						this.images.push({
+							media_id: response.result._id,
+							file_name: response.result.name,
+							sequence_number: this.addmediaForm.value.sequence_number
+						});
+						this.mediaData = response.result;
+						this.mediaFile = '';
+						this.isUploaded = false;
+						this.addmediaForm = this.formBuilder.group({
+							name: ['', Validators.required],
+							status: [true, Validators.required],
+							sequence_number: [''],
+							src: ['', Validators.required],
+							format: [''],
+							file_type: ['image'],
+							alt: [''],
+							role: [''],
+							resolution: [''],
+							size: [''],
+							height: [''],
+							width: [''],
+							mute: ['muted'],
+							autoplay: [true],
+							loop: [true],
+							full_screen: [''],
+						});
+						this.modalService.dismissAll();
+					}
+					else {
+						this.toastr.errorToastr(response.message);
+					}
+				},
+			);
+		}
+		else {
+			if (id) {
+				this.mediaService.editMediadata(obj, id).subscribe(
+					(response) => {
+						if (response.code == 200) {
+							this.throw_msg = response.message
+							this.msg_success = true;
+							this.toastr.successToastr(response.message);
+							if (this.mediaData) {
+								this.deletedMediaFile.push(this.mediaData.src);
+								this.deleteMediaFile();
+							}
+							setTimeout(() => {
+								this.mediaData.src = response.result.src;
+								window.location.reload();
+							}, 1000);
+							this.mediaData.src = response.result.src;
+							if (this.bannerData.media_data && this.bannerData.media_data.length > 0) {
+								this.patchingdata(this.id);
+							}
+							this.modalService.dismissAll();
+						} else {
+							this.throw_msg = response.message
+							this.msg_danger = true;
+							this.toastr.errorToastr(response.message);
+						}
+					},
+				);
+			}
+		}
+	}
+
+	onCancelMedia() {
+		this.addmediaForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			status: [true, Validators.required],
+			sequence_number: [''],
+			src: ['', Validators.required],
+			format: [''],
+			file_type: ['image'],
+			alt: [''],
+			role: [''],
+			resolution: [''],
+			size: [''],
+			height: [''],
+			width: [''],
+			mute: ['muted'],
+			autoplay: [true],
+			loop: [true],
+			full_screen: [''],
+		});
+		this.modalService.dismissAll();
+		this.deletedMediaFile.push(this.mediaFile);
+		this.deleteMediaFile();
+	}
+
+	deleteMediaFile() {
+		if (this.isUploaded && this.deletedMediaFile && this.deletedMediaFile.length > 0) {
+			let obj = {};
+			obj['files'] = this.deletedMediaFile;
+			this.mediaService.deletefile(obj).subscribe(
+				(response) => {
+					if (response.code == 200) {
+						this.isUploaded = false;
+						this.mediaFile = '';
+						this.deletedMediaFile = [];
+					}
+				},
+			);
+		}
+	}
+
 }
